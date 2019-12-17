e8762e442744f0bf34a56cc46d54f6fe4812a1c4
trevorpe <t.a.profitt@gmail.com>
2019-12-17 11:37:48 -0400

    Set version to 1.0

--- atomic_images/__init__.py
+++ atomic_images/__init__.py
@@ -2,7 +2,7 @@
 """
 
 __author__ = 'Trevor Profitt'
-__version__ = '0.6.3'
+__version__ = '1.0'
 __email__ = 'tprofitt@upei.ca'
 __program__ = 'atomic-images'
-__description__ = 'Python modules to create and use the atomic images representations'
+__description__ = 'Python modules to create and use ASCF representations with Keras'
